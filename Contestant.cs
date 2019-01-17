8b6fcd93c0ae96f1a9bd83735346d78ef37244ae
Matthew Nelson <mnelson251@gmail.com>
2019-01-17 08:43:03 -0600

    init commit

---
+++ Contestant.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    class Contestant : IContestant
+    {
+
+        public string FirstName;
+        public string LastName;
+        public string EmailAddress;
+        public int RegistrationNumber; // this needs to auto populate when contestant registers for sweepstakes
+
+        public Contestant()
+        {
+
+        }
+
+        public void CaptureContestantFirstName()
+        {
+
+        }
+
+        public void CaptureContestantLastName()
+        {
+
+        }
+
+        public void CaptureContestantEmail()
+        {
+
+        }
+
+
+        
+
+
+
+
+
+
+
+
+
+
+    }
+}
